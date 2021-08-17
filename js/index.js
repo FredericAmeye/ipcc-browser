7f1db62cd70799531f2718986925d896662c368c
Frederic Ameye <frederic.ameye@infoclimat.fr>
2021-08-17 20:05:59 +0200

    cache bump

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ let initFn = (function()
     }
 
     /* init page */
-    jQuery.getJSON('content/wgI.json?v2.json', function(r){
+    jQuery.getJSON('content/wgI.json?v3.json', function(r){
         const nb_chap = r.SPM.chapters.length;
         wgI = r;
         
